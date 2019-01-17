3848387e689d21df4f50b1b1a8ea865e4fc1d10e
Tom Kelley <th.kelley@northeastern.edu>
2019-01-17 14:29:11 -0500

    Update Jan 17

--- assignments/problem4/loadtext.py
+++ assignments/problem4/loadtext.py
@@ -1,9 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 16 21:43:20 2019
+# Starting program for Jan. 17
 
-@author: Tom K
-"""
 import numpy as np
 
 test=np.loadtxt("smallperiodictable.txt", dtype={'names': ('AN','name', 'symbol', 'AW','density','isotopes','year'), 'formats': ('i1', 'U16', 'U2','f4','f4','i4','i4')})
