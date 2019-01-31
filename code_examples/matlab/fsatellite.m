c0ed756ad1a800f41b74fe468f3bf6f85ace07ac
Tom Kelley <th.kelley@northeastern.edu>
2019-01-31 14:31:37 -0500

    Jan 31 Update

--- code_examples/matlab/fsatellite.m
+++ code_examples/matlab/fsatellite.m
@@ -5,11 +5,12 @@ function [h,v] = fsatellite(T,unit)
 % seconds, and "dy" for days. The function returns the altitude in km and
 % velocity in km/s
  
-if nargin==1
+if nargin==1 % nargin stores the number of arguments put in with the function
     unit= "hr";
 elseif nargin ==0 
     error("Invalid input")    
 end
+% Defining constants
 mts = 60;
 hts= 3600;
 dts = 86400;
@@ -19,7 +20,7 @@ R = 6.371*10^(6);
 % Calculations using anonymous functions!!
 V = @(r) sqrt(G*M./r);
 H = @(t) (G*M*t.^2./(4*pi.^2)).^(1/3) - R;
-
+% Checks the unit input
 switch unit
     case "sec"
         t=T;
@@ -40,11 +41,12 @@ switch unit
     otherwise
         error("Invalid input")
 end
-
+% Calculate altitude and velocity
 h = mtokm(H(t));
 v = mtokm(V(R+H(t)));
 time = 5200:100:8*t;
 height = 0:100:H(time(end));
+% Graph results in 2 figures
 figure
 plot(time/tconvert,mtokm(H(time)),'b-',t/tconvert,h,'r*')
 ylabel("altitude (km)")
@@ -58,9 +60,11 @@ ylabel('velocity in orbit (km/s)')
 title('Orbital Velocity')
 
     function H = mtokm(h)
+        % Converts input from meters to kilometers
         H = h./1000;
     end
     function h = kmtom(H) 
+        % Converts input from kilometers to meters
         h = 1000.*H;
     end
 end
